6fd244410a602d64e609a81e015c4ef33bd6c11c
FIRASING <mohamedfiras.bouzouraa@esprit.tn>
2025-03-14 12:31:07 +0100

    troisieme commit

--- src/app/Residence/residences/residences.component.ts
+++ src/app/Residence/residences/residences.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Residence } from 'src/app/core/Models/residence';
+import { Residence } from 'src/app/core/models/residence';
+import { ResidenceService } from 'src/app/core/services/residence.service';
+
 
 @Component({
   selector: 'app-residences',
@@ -7,42 +9,31 @@ import { Residence } from 'src/app/core/Models/residence';
   styleUrls: ['./residences.component.css']
 })
 export class ResidencesComponent {
-  listResidences:Residence[]=[
-    {id:1,"name": "El fel","address":"Borj Cedria",
-    "image":"../../assets/images/R1.jpg", status: "Disponible"},
-    {id:2,"name": "El yasmine",
-    "address":"Ezzahra","image":"../../assets/images/R2.jpg", status:
-    "Disponible" },
-    {id:3,"name": "El Arij",
-    "address":"Rades","image":"../../assets/images/R3.jpg", status:
-    "Vendu"},
-    {id:4,"name": "El Anber","address":"inconnu",
-    "image":"../../assets/images/R4.jpg", status: "En Construction"}
-    ];
-    selectedResidenceId: number | null = null;
-    showadd(R:Residence)
-    { if (R.address== 'inconnu')
-       {alert('inconnu');
 
-       }else  this.selectedResidenceId = this.selectedResidenceId === R.id ? null : R.id;
-    }
-    getStatusIcon(status: string) {
-      switch (status) {
-        case 'Disponible': return '✅';
-        case 'En Construction': return '🚧';
-        case 'Vendu': return '❌';
-        default: return '';
-      } 
-    }favorites: Residence[] = [];
-    addToFavorites(residence: Residence) {
-      if (!this.favorites.includes(residence)) {
-        this.favorites.push(residence);
-      }
-    }
-    searchText: string = '';
-        get filteredResidences() {
-          return this.listResidences.filter(residence =>
-            residence.address.toLowerCase().includes(this.searchText.toLowerCase())
-          );
-        }
+  listResidences:Residence[]=[ ];
+ showA=false;
+ searchItem= "";
+
+ constructor(private resServ:ResidenceService){}
+
+ ngOnInit(){
+
+   this.resServ.getResidences().subscribe(
+      data => this.listResidences = data,
+      erreur => console.log(erreur),
+      ( ) => console.log("Le chargement des résidences est terminé")
+   );
+}
+
+
+ show(R:Residence){
+  if (R.address=="inconnu"){alert("Adresse inconnue")}
+  else {this.showA=true;}
+ }
+ delete(id:number){
+  this.resServ.deleteResidence(id).subscribe(()=>{
+    this.resServ.getResidences().subscribe(
+     data => this.listResidences = data)
+  });
+ }
 }
